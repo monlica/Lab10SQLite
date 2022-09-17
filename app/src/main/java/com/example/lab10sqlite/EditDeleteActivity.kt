1b20792a97c4a9b5b92e67ea842de4fa51b172d5
monlica <wattana.mcs@gmail.com>
2022-09-17 23:56:47 +0700

    ok
    ok

---
+++ app/src/main/java/com/example/lab10sqlite/EditDeleteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab10sqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditDeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_delete)
+    }
+}
